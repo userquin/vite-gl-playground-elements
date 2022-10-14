0eca7d8399d4becf9578fe065c97620418fe78a2
userquin <userquin@gmail.com>
2022-10-14 19:32:05 +0200

    chore: add minimal example

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import sandbox from 'virtual:pg-sandbox'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <playground-ide editable-file-system line-numbers resizable sandbox-base-url="${sandbox}">
-    
+        <script type="sample/html" filename="index.html">
+<!doctype html>
+<body>
+  <script src="./index.js">&lt;/script>
+</body>
+        </script>
+        <script type="sample/ts" filename="index.ts">
+    document.body.textContent = 'Hello!';
+        </script>
     </playground-ide>
   </div>
 `
